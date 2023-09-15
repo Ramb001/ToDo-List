0078ded955138ecfa32804b3954e2f89528ae755
Artem <artem.rmb@bk.ru>
2023-09-15 20:47:21 +0300

    feat: created component for todo item

---
+++ src/components/ToDoItem/index.tsx
@@ -0,0 +1,47 @@
+import { CheckCircle, CircleOutlined } from "@mui/icons-material";
+import styles from "./ToDoItem.module.scss";
+import { Checkbox } from "@mui/material";
+import { useState } from "react";
+
+type objType = {
+  title: string;
+  status: "done" | "progress";
+};
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+function ToDoItem({ props }: { props: objType }) {
+  const [status, setStatus] = useState(props.status);
+
+  function changeStatus(bool: boolean) {
+    if (bool) {
+      setStatus("done");
+    } else {
+      setStatus("progress");
+    }
+  }
+
+  return (
+    <div className={styles.box}>
+      <Checkbox
+        {...label}
+        color="success"
+        icon={<CircleOutlined className={styles.icon} />}
+        checkedIcon={<CheckCircle className={styles.icon} />}
+        onChange={(e) => {
+          changeStatus(e.target.checked);
+        }}
+      />
+      <div
+        style={{
+          textDecoration: status === "done" ? "line-through" : "",
+          color: status === "done" ? "gray" : "black",
+        }}
+      >
+        {props.title}
+      </div>
+    </div>
+  );
+}
+
+export default ToDoItem;
