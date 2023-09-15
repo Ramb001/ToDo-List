bdf08e09412301891af62f0c742ce053814a7900
Artem <artem.rmb@bk.ru>
2023-09-15 22:00:36 +0300

    feat: added new component on a home page and styles it

--- src/pages/HomePage/HomePage.module.scss.d.ts
+++ src/pages/HomePage/HomePage.module.scss.d.ts
@@ -2,3 +2,5 @@ export const main: string;
 export const list: string;
 export const input: string;
 export const title: string;
+export const inputBox: string;
+export const arrowBox: string;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styles from "./HomePage.module.scss";
 import ToDoItem from "../../components/ToDoItem";
+import DownBar from "../../components/DownBar";
+import DownArrow from "../../assets/icons/DownArrow";
 
 type objType = {
   title: string;
@@ -10,6 +12,8 @@ type objType = {
 function HomePage() {
   const [data, setData] = useState<Array<objType>>([]);
   const [title, setTitle] = useState("");
+  const [status, setStatus] = useState<"all" | "active" | "completed">("all");
+  const [view, setView] = useState<boolean>(true);
 
   function addItem() {
     if (title.length !== 0) {
@@ -24,19 +28,39 @@ function HomePage() {
     <div className={styles.main}>
       <div className={styles.list}>
         <h1 className={styles.title}>todos</h1>
-        <input
-          type="text"
-          placeholder="What needs to be done?"
-          className={styles.input}
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          onBlur={addItem}
-        />
+        <div className={styles.inputBox}>
+          <div onClick={() => setView(!view)} className={styles.arrowBox}>
+            <DownArrow />
+          </div>
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            className={styles.input}
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            onBlur={addItem}
+          />
+        </div>
         {data.map((obj: objType, i) => {
-          return <ToDoItem props={obj} key={i} />;
+          if (
+            view &&
+            ((status === "active" && obj.status == "progress") ||
+              (status === "completed" && obj.status == "done") ||
+              status === "all")
+          ) {
+            return <ToDoItem props={obj} key={i} />;
+          }
         })}
+        <DownBar
+          props={{
+            data: data,
+            setData: setData,
+            status: status,
+            setStatus: setStatus,
+          }}
+        />
       </div>
     </div>
   );
