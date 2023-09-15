1b96811b69bd25006ed245be4e3384ec93983eb4
Artem <artem.rmb@bk.ru>
2023-09-15 19:23:12 +0300

    feat: created folder for components and created home page

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./HomePage.module.scss";
+
+function HomePage() {
+  return (
+    <div className={styles.main}>
+      <div></div>
+    </div>
+  );
+}
+
+export default HomePage;
