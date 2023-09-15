de29a9bb4eccfe8072afb1b4d0f774f396cf7ade
Artem <artem.rmb@bk.ru>
2023-09-15 20:46:57 +0300

    feat: created list of todos and styles it

--- src/pages/HomePage/HomePage.module.scss.d.ts
+++ src/pages/HomePage/HomePage.module.scss.d.ts
@@ -1,1 +1,4 @@
 export const main: string;
+export const list: string;
+export const input: string;
+export const title: string;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
 import styles from "./HomePage.module.scss";
+import ToDoItem from "../../components/ToDoItem";
 
-type dataType = {
+type objType = {
   title: string;
   status: "done" | "progress";
 };
 
 function HomePage() {
-  const [data, setData] = useState<Array<dataType>>([]);
+  const [data, setData] = useState<Array<objType>>([]);
+  const [title, setTitle] = useState("");
+
+  function addItem() {
+    if (title.length !== 0) {
+      let temp = [...data];
+      temp.push({ title: title, status: "progress" });
+      setData(temp);
+      setTitle("");
+    }
+  }
 
   return (
     <div className={styles.main}>
-      <div></div>
+      <div className={styles.list}>
+        <h1 className={styles.title}>todos</h1>
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          className={styles.input}
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          onBlur={addItem}
+        />
+        {data.map((obj: objType, i) => {
+          return <ToDoItem props={obj} key={i} />;
+        })}
+      </div>
     </div>
   );
 }
