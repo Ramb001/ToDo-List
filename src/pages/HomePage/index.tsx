3cc757fe5281153db34d1f2ba4206404ff1827c6
Artem <artem.rmb@bk.ru>
2023-09-15 19:28:28 +0300

    feat: created array for todos and type for data

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import styles from "./HomePage.module.scss";
 
+type dataType = {
+  title: string;
+  status: "done" | "progress";
+};
+
 function HomePage() {
+  const [data, setData] = useState<Array<dataType>>([]);
+
   return (
     <div className={styles.main}>
       <div></div>
